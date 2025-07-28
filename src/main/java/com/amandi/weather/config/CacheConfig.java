262096bf1f7c2403c38cf7c79eab2a3f284a5c7a
amandi <amandiimasha43@gmail.com>
2025-07-28 22:58:00 +0530

    fix bugs

--- src/main/java/com/amandi/weather/config/CacheConfig.java
+++ src/main/java/com/amandi/weather/config/CacheConfig.java
@@ -2,7 +2,6 @@ package com.amandi.weather.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +9,15 @@ import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-@EnableCaching
 public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("weatherCache");
-        cacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES));
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("weather");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .maximumSize(100));
+        cacheManager.setAsyncCacheMode(true); //
         return cacheManager;
     }
 }
-
