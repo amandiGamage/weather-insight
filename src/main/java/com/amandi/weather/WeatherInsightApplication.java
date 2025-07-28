72c4f82bbb510e8d50e4caf989b9e66e51f89e45
amandi <amandiimasha43@gmail.com>
2025-07-28 20:39:02 +0530

    Add cache and async configuration

--- src/main/java/com/amandi/weather/WeatherInsightApplication.java
+++ src/main/java/com/amandi/weather/WeatherInsightApplication.java
@@ -2,12 +2,16 @@ package com.amandi.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Main class for the Weather Insight Spring Boot application.
  * Enables caching and asynchronous processing.
  */
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class WeatherInsightApplication {
 
     public static void main(String[] args) {
